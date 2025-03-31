62912efe81423e01d85acee5175c97cd0c9ca8b5
gianlucaastorino <gianluca@symplistic.ai>
2025-03-31 12:35:45 -0400

    everything

---
+++ assets/js/reusable.js
@@ -0,0 +1,13 @@
+// footer
+fetch('../footer.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('footer-reusable-container').innerHTML = html;
+    });
+
+// navbar
+fetch('../nav.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('nav-reusable-container').innerHTML = html;
+    });
